ae3aa3446a07d3eecb302d63bc0b705ed9899ddd
smilesmilehero <jordy827@yahoo.com.tw>
2020-08-18 16:16:55 +0800

    show card result

--- credit_card/app/src/main/java/com/example/credit_card/MainActivity.java
+++ credit_card/app/src/main/java/com/example/credit_card/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.credit_card;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -56,7 +57,6 @@ public class MainActivity extends AppCompatActivity{
                 // 宣告執行緒
                 Thread thread = new Thread(mutiThread);
                 thread.start(); // 開始執行
-
 
             }
         });
@@ -137,6 +137,9 @@ public class MainActivity extends AppCompatActivity{
             }
             income = input_salary.getText().toString();
             show1.setText(income);
+
+            startActivity(new Intent(MainActivity.this, fake_data.class));
+
         }
     };
 }
