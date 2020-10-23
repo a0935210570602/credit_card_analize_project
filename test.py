c07f8c96f09cfe775837a653fa916cee59c48766
clarkiron <65802679+clarkiron@users.noreply.github.com>
2020-10-23 20:01:02 +0800

    Add files via upload
    code for extracting data from excel and store them into mongo

--- test.py
+++ test.py
@@ -1,28 +1,23 @@
-from pymongo import MongoClient
+from pymongo import MongoClient # for database
+import xlrd #for excel
+ 
+client = MongoClient() #connect mongo
+db = client.test #access database
+posts = db.posts #collection of test
 
-import xlrd
+loc = ("credit_card_sample.xlsx")#loaction of the file
+wb = xlrd.open_workbook(loc)#open file
+sheet = wb.sheet_by_index(0)#???? 
  
-client = MongoClient()
-#connect mongo
-db = client.test
-#access database
-posts = db.posts
-#????
-
-loc = ("credit_card_sample.xlsx")
- 
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
- 
-sheet.cell_value(0, 0)
+sheet.cell_value(0, 0)#?????
  
 for i in range(sheet.nrows):
-    if i == 0:
+
+    if i == 0:#skip the first row
         continue
     
     list = sheet.row_values(i)
-    print(list[1])
-
+    print(list)#check every row value
     post_data = {
     'Name of the Bank': list[1],
     'Credit Card': list[2],
@@ -31,5 +26,10 @@ for i in range(sheet.nrows):
     'Dosmestic Cashback': list[5],
     }
 
-    result = posts.insert_one(post_data)#??
-    print('One post: {0}'.format(result.inserted_id))#???
+    result = posts.insert_one(post_data)#insert the value to database
+    print('One post: {0}'.format(result.inserted_id))#print out the id
+
+
+#two lines below are used to find a specific data
+#scotts_posts = posts.find({'Credit Card':'現金回饋PLUS卡'})
+#print(scotts_posts)
