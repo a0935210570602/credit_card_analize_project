716f3693d8541b141e59d73bb3d68100563005fb
a0935210570602 <a0935210570602@gmail.com>
2020-11-01 10:07:39 +0800

    aa

--- Server/NPM_analist/getdata.py
+++ Server/NPM_analist/getdata.py
@@ -8,8 +8,8 @@ import requests
 headers = {
    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.41'
    }
-restaurant_name = "屋馬2" #6207評論 #存擋用
-url_prefix = "https://www.google.com/async/reviewSort?vet=12ahUKEwiL1Zegx9rsAhXiyIsBHbdnBUcQxyx6BAgBECQ..i&ved=2ahUKEwiL1Zegx9rsAhXiyIsBHbdnBUcQjit6BQgBEIMB&rlst=f&rlz=1C1CHBF_zh-TWTW910TW910&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!2m1!1e3!3sIAE,lf:1,lf_ui:4&yv=3&async=feature_id:0x346915e562d64d3b%3A0x221ad56de53b5cb7,review_source:All%20reviews,sort_by:"
+restaurant_name = "饗食天堂_台北京站店" #6207評論 #存擋用
+url_prefix = "https://www.google.com/async/reviewSort?vet=12ahUKEwiz85Pt0d_sAhXTIqYKHSEWCaEQxyx6BAgBEC0..i&ved=2ahUKEwiz85Pt0d_sAhXTIqYKHSEWCaEQjit6BQgBEIgB&rlz=1C1CHBF_zh-TWTW910TW910&yv=3&async=feature_id:0x3442a96d74fc20d5%3A0x2a9bea92dc76f286,review_source:All%20reviews,sort_by:"
 #建立author, grade, comment來存放資料
 author =[]
 grade = []
@@ -17,17 +17,18 @@ comment = []
 sorting_method = "newestFirst,"  #排序方式用最新
 
 
-for i in range(0,366):  #由上圖，判斷range上限，控制在小於等於 <= [(最大評論數/10)-1]
+for i in range(0,260):  #由上圖，判斷range上限，控制在小於等於 <= [(最大評論數/10)-1]
     start_index_value = i*10 #開始評論序
     next_page_token_value = start_index_value +10 #下一頁評論序
 #把完整url組裝起來
-    url =  url_prefix + sorting_method + "start_index:" + str(start_index_value) + ",is_owner:false,filter_text:,associated_topic:,next_page_token:" + "CgIICg"  + ",_pms:s,_fmt:pc"
-             
+    url =  url_prefix + sorting_method + "start_index:" + str(start_index_value) + ",is_owner:false,filter_text:,associated_topic:,next_page_token:" + str(next_page_token_value)  + ", _pms:s,_fmt:pc"
+    # print("url\n",url)
 # 發送get請求
     sleep_time=random.uniform(0,2)
     print(f"先停留{sleep_time}秒...")
     time.sleep(sleep_time)
     text = requests.get(url,headers=headers).text
+    # print("text",text)
     soup = BeautifulSoup(text,'lxml')
     
     for s in soup.find_all(class_='jxjCjc'):
@@ -37,7 +38,7 @@ for i in range(0,366):  #由上圖，判斷range上限，控制在小於等於 <
             #print(s.find(class_="Jtu6Td").text)
                 comment.append(s.find(class_="Jtu6Td").text)
                 author.append(s.find(class_="TSUbDb").text)
-                #print(author)
+                # print("author",author)
                 grade.append(s.find(class_='Fam1ne')['aria-label'].split("：")[1].split(" ")[0])
             #print(grade)
     print(f"已抓取完第{start_index_value}到{next_page_token_value}項目..")
