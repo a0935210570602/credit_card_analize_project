24ec0e73d3c3b81cd4e995dcd724906ace5276d4
clarkiron <65802679+clarkiron@users.noreply.github.com>
2020-11-02 00:19:58 +0800

    Add files via upload
    Lol

--- creat_database.py
+++ creat_database.py
@@ -12,7 +12,7 @@ if check == 'y':
     posts = db.posts #collection of test
     posts.drop()
 
-    loc = ("credit_card_sample.xlsx")#loaction of the file
+    loc = ("result_lst.xlsx")#loaction of the file
     wb = xlrd.open_workbook(loc)#open file
     sheet = wb.sheet_by_index(0)#???? 
  
@@ -31,6 +31,28 @@ if check == 'y':
         'Anual Fee': list[3],
         'Foreign Cashback': list[4],
         'Dosmestic Cashback': list[5],
+        'Foreign Mileage Credit(Percentage)': list[6],
+        'Dosmestic Mileage Credit(Percentage)': list[7],
+        'Foreign Mileage Credit(NT/mile)': list[7],
+        'Dosmestic Mileage Credit(NT/mile)': list[7],
+        'Refuel Cashback': list[8],
+        'Refuel Discount': list[9],
+        'Weekend Movie Discount': list[10],
+        'Week days Movie Discount': list[11],
+        'Highest Installment': list[12],
+        'Lowest Installment Amount': list[13],
+        'Maxium Cashback': list[14],
+        'Online Shopping Feedback': list[15],
+        'Churning Suitcase': list[16],
+        'Churning Mileage Credit': list[17],
+        'Churning Cashback': list[18],
+        'Credit Card Fee Mileage Credit': list[19],
+        'Highest Installment(0 Interest)': list[20],
+        'Credit Card Fee Cashback': list[21],
+        'Lowest Cost Qualification for Installment': list[22],
+        'Credits for Cash': list[23],
+        'Credits fot Mileage': list[24],
+        'Credit Card Multiple Points': list[25],
         }
 
         result = posts.insert_one(post_data)#insert the value to database
